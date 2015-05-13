e152fa1590d3992bb62217381130cdd3a485f890
soliz0 <soliz0@gmail.com>
2015-05-13 18:14:41 -0300

    inicio internacionalizacao

--- js/savenergy.js
+++ js/savenergy.js
@@ -70,8 +70,28 @@ $(document).ready(function () {
 
     }(jQuery);
 
-
-
 });
+
+//$(document).ready(function() {
+//                loadBundles($.i18n.browserLang());
+//                // configure language combo box
+//                $('#sidebar').change(function() {
+//                    var selection = $('#sidebar option:selected').val();
+//                    loadBundles(selection !== 'browser' ? selection : $.i18n.browserLang());
+//                });
+//            });
+//            function loadBundles(lang) {
+//                $.i18n.properties({
+//                    name: 'Messages',
+//                    path: 'bundle/',
+//                    mode: 'both',
+//                    language: lang,
+//                    callback: function() {
+//                        //$("#msg_welcome").text($.i18n.prop('msg_welcome'));
+//                        $("#msg_welcome").text(msg_welcome);
+//                        $("#msg_selLang").text(msg_selLang(lang));
+//                    }
+//                });
+//            }
 
 
