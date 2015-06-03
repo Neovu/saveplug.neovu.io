9d92892fcce8551d432db10b18757e9bd00f3df7
schmaedech <schmaedech@gmail.com>
2015-06-03 01:44:28 -0300

    bower

---
+++ mail/send_mail.php
@@ -0,0 +1,27 @@
+<?php
+define('EMAIL_DEFAULT_FROM_ADDRESS', 'webmaster@savenergy.me');
+define('EMAIL_DEFAULT_FROM_NAME', 'Myself');
+define('FORM_BLOCK_SENDER_EMAIL', EMAIL_DEFAULT_FROM_ADDRESS);
+if (isset($_SERVER['HTTP_X_REQUESTED_WITH'])) {
+    if (isset($_POST['name']) AND isset($_POST['email']) AND isset($_POST['subject']) AND isset($_POST['message'])) {
+        $to = 'contato@savenergy.me';//please put ours.
+        $from = 'webmaster@savenergy.me';//please put ours.
+        
+        $name = filter_var($_POST['name'], FILTER_SANITIZE_STRING);
+        $email = filter_var($_POST['email'], FILTER_SANITIZE_EMAIL);
+        $subject = filter_var($_POST['subject'], FILTER_SANITIZE_STRING);
+        $message = filter_var($_POST['message'], FILTER_SANITIZE_STRING);
+
+        
+        $subject = 'From: ' . $email . ' : ' . $subject;
+        if (mail($to, $subject, $message, 'From: ' . $from)) {
+            echo 'sucess|Obrigado, '.$name.'. Retornaremos em assim que possível.';
+        } else {
+            echo 'erro|Opa, '.$name.' aconteceu algum problema no envio. Tente direto pelo email contato@savenergy.me!';
+        }
+    } else {
+        echo 'error|Por favor, '. $name.' todos os campos necessários.';
+    }
+    return ;
+}
+?>
